8552b4418513d0855e8ffd5023086a190a0beba7
Navin Kumaran <navinkumaran2004@gmail.com>
2024-08-05 20:26:54 +0530

    setting

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ app.use("/api/semesters", semesterRoutes);
 app.use("/api/verticals", verticalRoutes);
 app.use("/api/students", studentRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
