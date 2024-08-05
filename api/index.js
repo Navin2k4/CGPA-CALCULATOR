21e47b70578f8957ae1e016c86d24fd2c4a8521b
Navin Kumaran <navinkumaran2004@gmail.com>
2024-08-05 20:11:35 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -1,36 +1,33 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-import departmentRoutes from './routes/department.route.js';
-import semesterRoutes from './routes/semester.route.js';
-import verticalRoutes from './routes/vertical.route.js';
-import studentRoutes from './routes/student.route.js';
+import departmentRoutes from "./routes/department.route.js";
+import semesterRoutes from "./routes/semester.route.js";
+import verticalRoutes from "./routes/vertical.route.js";
+import studentRoutes from "./routes/student.route.js";
 
-import path from 'path';
+import path from "path";
 
 dotenv.config();
 
 mongoose
-    .connect(process.env.MONGO)
-    .then(() => {
-        console.log("MongoDB is Connected");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
-const __dirname = path.resolve();
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.use(express.json());
 
-
-app.use('/api/departments', departmentRoutes);
-app.use('/api/semesters', semesterRoutes);
-app.use('/api/verticals', verticalRoutes);
-app.use('/api/students', studentRoutes);
+app.use("/api/departments", departmentRoutes);
+app.use("/api/semesters", semesterRoutes);
+app.use("/api/verticals", verticalRoutes);
+app.use("/api/students", studentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
@@ -41,15 +38,15 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 3000;
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
